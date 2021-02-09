889795e367c07b09b6f1ba648e220fb54a56e21a
Marina Ranenko <mranenko@gmail.com>
2021-02-09 09:44:27 -0700

    Added images, styles, and a click event handler

--- scripts/App.js
+++ scripts/App.js
@@ -1,6 +1,24 @@
 class App {
   static start() {
-    console.log('App start');
+    const photos = document.querySelectorAll('.photo');
+
+    /* clear active (expanded) photos */
+    const photosClearActive = () => {
+      photos.forEach(photo => {
+        photo.classList.remove('photo-active');
+      });
+    };
+
+    /* assign a click event to each photo */
+    photos.forEach(photo => {
+      photo.addEventListener('click', () => {
+        photosClearActive();
+        photo.classList.add('photo-active');
+      });
+    });
+
+    /* set the first photo to active (expanded) */
+    photos[0].classList.add('photo-active');
   }
 }
 
